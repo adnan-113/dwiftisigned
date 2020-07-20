4c319ae7c25d21b82aea0bd7862ef3ed7eb9161a
DwifteJB <dwiftedisc@gmail.com>
2020-07-21 00:34:26 +0100

    cool release

---
+++ Updater.sh
@@ -0,0 +1,10 @@
+echo Starting
+  git add .
+   git commit -m "Updating Files.. (I am a bot, I have no idea what is happening...)"
+    echo — - - - - - - - - - - - - - - -
+    echo Uploading to Github! Approx 1m
+    echo — - - - - - - - - - - - - - - -
+    git push
+    echo — - - - - - - - - 
+    echo Pushed to Github! 
+    echo — - - - - - - - - 
