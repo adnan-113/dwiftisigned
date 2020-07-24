4a05de347067e235b63fb363aec6941a255ee7e9
DwifteJB <dwiftedisc@gmail.com>
2020-07-24 12:58:18 +0100

    Added u0 5.3.1 with 7 certs!!

--- Updater.sh
+++ Updater.sh
@@ -18,7 +18,7 @@ cd /var/mobile/Documents/git/dwiftisigned
 
 echo Starting
   git add .
-   git commit -m "Updated Files. Check out the website for changes!"
+   git commit -m "Added u0 5.3.1 with 7 certs!!"
     echo — - - - - - - - - - - - - - - -
     echo Uploading to Github! Approx 1m
     echo — - - - - - - - - - - - - - - -
