99101bc818fdf77662a206c37690fbfc6b92d876
DwifteJB <dwiftedisc@gmail.com>
2020-07-24 13:58:22 +0100

    Fixed Profile x2

--- Updater.sh
+++ Updater.sh
@@ -18,7 +18,7 @@ cd /var/mobile/Documents/git/dwiftisigned
 
 echo Starting
   git add .
-   git commit -m "Fixed Profile"
+   git commit -m "Fixed Profile x2"
     echo — - - - - - - - - - - - - - - -
     echo Uploading to Github! Approx 1m
     echo — - - - - - - - - - - - - - - -
