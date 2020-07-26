68f5fb4edbf843afdc7424d96a59a7d8513d41e8
DwifteJB <dwiftedisc@gmail.com>
2020-07-26 13:28:57 +0100

    prob fixed odyssey download

--- Updater.sh
+++ Updater.sh
@@ -18,7 +18,7 @@ cd /var/mobile/Documents/git/dwiftisigned
 
 echo Starting
   git add .
-   git commit -m "Added Geometry Dash"
+   git commit -m "prob fixed odyssey download"
     echo — - - - - - - - - - - - - - - -
     echo Uploading to Github! Approx 1m
     echo — - - - - - - - - - - - - - - -
